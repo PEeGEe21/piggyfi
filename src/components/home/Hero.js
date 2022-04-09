d28665d2ec452123a74e06fb45fde599d4975ff1
= <=>
2022-04-09 14:01:56 +0100

    edited piggyfi

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -11,13 +11,13 @@ export default function Hero() {
 
             <div className="container w-full">
                 <main className="sm:px-8 px-[10px]   ">
-                    <div className="pt-24 pb-24 mx-auto  flex flex-col md:flex-row sm:flex-col   px-4 ">
+                    <div className="pt-5 pb-24 mx-auto  flex flex-col md:flex-row sm:flex-col justify-center items-center px-4 ">
                     <div className="flex-1">
                         <div className="text-left">
                         <h1 className={styles.heading_title}>
                             A smart way to save money across Africa
                         </h1>
-                        <p className="mt-3 mx-auto text-base text-white sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                        <p className="mt-3 mr-10 max:width-auto mx-auto text-base text-white sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                             PiggyFi makes use of the Blockchain to allow for easy and automated peer-to-peer money transfers within and  outside of Africa, as well as saving in US Dollars to protect against inflation and depreciation.
                         </p>
 
@@ -43,6 +43,54 @@ export default function Hero() {
                     </div>
 
                     <div className="flex-1  w-full text-center">
+                        <div className={styles.gallery}>
+                            <div class={styles.row}>
+                                <div class={styles.column}>
+                                    
+                                    {/* <Image src="/images/gallery/pic1.png" objectFit="contsin" width="100%" height="100%" />
+                                    <Image src="/images/gallery/pic2.png" objectFit="cover" layout="fill" />
+                                    <Image src="/images/gallery/pic3.png" objectFit="cover" layout="fill" />
+                                    <Image src="/images/gallery/pic4.png" objectFit="cover" layout="fill" /> */}
+                                    <img src="/images/gallery/pic1.png"/>
+                                    <img src="/images/gallery/pic2.png"/>
+                                    <img src="/images/gallery/pic3.png"/>
+                                    <img src="/images/gallery/pic4.png"/>
+                                </div>
+                                <div class={styles.column}>
+                                    <img src="/images/gallery/pic5.png"/>
+                                    <img src="/images/gallery/pic6.png"/>
+                                    <img src="/images/gallery/pic7.png"/>
+                                    <img src="/images/gallery/pic8.png"/>
+                                </div>
+                                <div class={styles.column}>
+                                    <img src="/images/gallery/pic9.png"/>
+                                    <img src="/images/gallery/pic10.png"/>
+                                    <img src="/images/gallery/pic11.png"/>
+                                    <img src="/images/gallery/pic12.png"/>
+                                </div>
+                                <div class={styles.column}>
+                                    <img src="/images/gallery/pic13.png"/>
+                                    <img src="/images/gallery/pic14.png"/>
+                                    <img src="/images/gallery/pic15.png"/>
+                                    <img src="/images/gallery/pic16.png"/>
+                                </div>
+                            </div>
+
+
+
+                            {/* <div class="grid grid-cols-4 grid-flow-col gap-4">
+                            <div class="row-span-3 relative"><Image src="/images/portrait-of-happy-black-family.png" objectFit="contain" layout="fill" width={50} height={50}/></div>
+                                <div>09</div>
+                                <div className="relative"><img src="/images/portrait-of-happy-black-family.png" width={50} height={50}/></div>
+                                <div>09</div>
+                            </div> */}
+                        </div>
+
+                        {/* <div class="grid grid-rows-3 grid-flow-col gap-4">
+                        <div class="row-span-3 ...">01</div>
+                        <div class="col-span-2 ...">02</div>
+                        <div class="row-span-2 col-span-2 ...">03</div>
+                        </div> */}
                         
                     {/* <div className="grid grid-rows-5 grid-flow-col gap-4 text-white">
                         
